78e25fc48f0740c04472fca65174ceb5def011d0
DESKTOP-N8J9AHQ\Keanu <supra4keanu@hotmail.com>
2018-06-19 03:49:02 +1000

    News Search Functionallity

---
+++ javascript/newsScript.js
@@ -0,0 +1,65 @@
+// Unapproved use and redistribution of this code and respective product is strictly prohibited.
+// Copyright© 2018 Keanu Ashwell all rights are reserved to the author, creator, registered 
+// and licensed owners of this product and it's content
+
+'use strict';
+
+// Add the event listner
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Search functionallity
+
+    // Add the event listner
+    document.getElementById("news-search").addEventListener('change', function() {
+
+        // Decalre and define variables
+        var search = document.getElementById("news-search").value.toUpperCase();
+        var newsContentContainer = document.getElementsByClassName('news-content-container');
+        var newsAuthor = document.getElementsByClassName("news-author");
+        var newsDate = document.getElementsByClassName("news-post-date");
+        var newsId = undefined;
+        var newsContent = document.getElementsByClassName("news-post-content");
+        var news;
+
+        // Make sure search has a value
+        if(search != "") {
+
+            // Loop through the posts
+            for(var i = 0; i < newsContent.length; i++) {
+
+                // Convert to uppercase this entry relative to the increment
+                var news = newsContent[i].textContent.toUpperCase();
+
+                // Check the posts for the search criteria
+                if(news.indexOf(search) > 0) {
+
+                    // Change the style
+                    document.getElementById("news-post-" + i).style.display = "grid";
+
+                } else {
+
+                    // Change the style
+                    document.getElementById("news-post-" + i).style.display = "none";
+
+                }
+
+            }
+
+        } else {
+            
+            // Loop through the posts
+            for(var i = 0; i < newsContent.length; i++) {
+
+                // Convert to uppercase this entry relative to the increment
+                var news = newsContent[i].textContent.toUpperCase();
+
+                // Change the style
+                document.getElementById("news-post-" + i).style.display = "grid";
+
+            }
+
+        }
+
+    }, true);
+    
+}, true);
