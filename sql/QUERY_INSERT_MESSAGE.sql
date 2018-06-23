a18a26fa37b86f47c14553955e0cdf10cf2a3d95
Unknown <supra4keanu@hotmail.com>
2018-06-24 08:20:18 +1000

    New SQL Additions

---
+++ sql/QUERY_INSERT_MESSAGE.sql
@@ -0,0 +1,12 @@
+INSERT INTO exchangeme.messages (
+exchangeme.messages.id,
+exchangeme.messages.sender,
+exchangeme.messages.date,
+exchangeme.messages.message
+)
+VALUES (
+DEFAULT,
+"Keanu Ashwell",
+DEFAULT,
+"Hello World"
+);
