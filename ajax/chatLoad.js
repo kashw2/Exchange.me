17e16c6e6ece18fb76feacbbd89491135709988f
Unknown <supra4keanu@hotmail.com>
2018-06-24 08:20:06 +1000

    AJAX Live chat loading

---
+++ ajax/chatLoad.js
@@ -0,0 +1,35 @@
+// Unapproved use and redistribution of this code and respective product is strictly prohibited.
+// Copyright© 2018 Keanu Ashwell all rights are reserved to the author, creator, registered 
+// and licensed owners of this product and it's content
+
+// Check the document is ready
+$(document).ready(function() {
+
+    // Check for keypress
+    $("#messaging-message").keypress(function(e) {
+
+        // Keycode 13 was pressed (enter)
+        if(e.which == "13" && $("#messaging-message").val() != "") {
+
+            $("#chat-messages").load("ajax/inc/chat.inc.insert", {
+                Message: $("#messaging-message").val(),
+                Username: $("#grid-content__loggedin").data("user")
+                }, 
+                
+                // Callback
+                function(responseTxt, statusTxt, xhr) {
+
+                    $("#messaging-message").val("");
+
+                }
+            
+            );
+
+        }
+
+        // Prevent key from repeating
+        // e.preventDefault();        
+
+    });
+
+});
