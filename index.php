7ad8fde41207e54de2a24a76dd672f8a2008571d
Unknown <supra4keanu@hotmail.com>
2018-06-25 11:15:53 +1000

    Altered to prevent news duplication bug

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Unapproved use and redistribution of this code and respective product is strictly prohibited.
-// Copyright© 2018 Keanu Ashwell all rights are reserved to the author, creator, registered 
+// Copyright© 2018 Keanu Ashwell all rights are reserved to the author, creator, registered
 // and licensed owners of this product and it's content
 
 // Start outputbuffer
@@ -290,11 +290,9 @@ require_once('mysql.php');
                     SELECT
                     exchangeme.news.author,
                     exchangeme.news.date,
-                    exchangeme.news.content,
-                    exchangeme.accounts.creationdate
+                    exchangeme.news.content
                     FROM 
-                    exchangeme.news,
-                    exchangeme.accounts
+                    exchangeme.news
                     ORDER BY exchangeme.news.id DESC;
 
                     ");
