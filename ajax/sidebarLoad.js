3683d82f1dba98beeab11833ffffcd8a3c29ddda
Unknown <supra4keanu@hotmail.com>
2018-07-04 23:50:58 +1000

    Account details change AJAX

--- ajax/sidebarLoad.js
+++ ajax/sidebarLoad.js
@@ -7,4 +7,25 @@
 // Check if document is ready
 $(document).ready(function() {
 
+    // Check for click
+    $("#options-save").on("click", function() {
+
+        // AJAX
+        $("#options-response").load("ajax/inc/account.inc.details.update.php", {
+            CurrentUsername: $("#grid-content__loggedin").data("user"),
+            Email: $("#options-input-email").val(),
+            Username: $("#options-input-username").val(),
+            Password: $("#options-input-password").val(),
+            Alias: $("#options-input-alias").val()
+            },
+
+            // Callback
+            function(responseText, statusText, xhr) {
+
+            }
+    
+        );
+
+    });
+
 });
