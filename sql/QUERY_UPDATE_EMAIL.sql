ae7f58b475bd94c0575a1816beb394c398f49729
Unknown <supra4keanu@hotmail.com>
2018-07-04 23:26:58 +1000

    Altered update email query

--- sql/QUERY_UPDATE_EMAIL.sql
+++ sql/QUERY_UPDATE_EMAIL.sql
@@ -1,3 +1,4 @@
 UPDATE exchangeme.accounts
 SET exchangeme.accounts.email = "Supra4keanu@hotmail.com"
-WHERE exchangeme.accounts.username = "Keanu Ashwell";
+WHERE exchangeme.accounts.username = "Keanu Ashwell"
+OR exchangeme.accounts.session = "9oi80raji73bjhtocs7tjh2te7";
