bf266c73ec409106da65a590330c0dfb745b0304
Unknown <supra4keanu@hotmail.com>
2018-07-05 14:41:40 +1000

    New Queries

---
+++ sql/QUERY_UPDATE_ALIAS.sql
@@ -0,0 +1,4 @@
+UPDATE exchangeme.accounts
+SET exchangeme.accounts.alias = "Keanu"
+WHERE exchangeme.accounts.username = "Keanu Ashwell"
+OR exchangeme.accounts.session = "9oi80raji73bjhtocs7tjh2te7";
---
+++ sql/QUERY_UPDATE_PASSWORD.sql
@@ -0,0 +1,4 @@
+UPDATE exchangeme.accounts
+SET exchangeme.accounts.password = "fcb4174b1c4f85c27027295ff78df21cÔ"
+WHERE exchangeme.accounts.username = "Keanu Ashwell"
+OR exchangeme.accounts.session = "9oi80raji73bjhtocs7tjh2te7";
