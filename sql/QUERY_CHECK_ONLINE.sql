4f26b1150bc7b82d825ddbe45eb71896075514e5
Unknown <supra4keanu@hotmail.com>
2018-07-15 03:19:45 +1000

    Query optimisation

--- sql/QUERY_CHECK_ONLINE.sql
+++ sql/QUERY_CHECK_ONLINE.sql
@@ -2,4 +2,4 @@ SELECT
 exchangeme.accounts.username,
 exchangeme.accounts.lastlogin
 FROM exchangeme.accounts
-WHERE exchangeme.accounts.lastlogin > DATE_SUB(NOW(), INTERVAL 5 MINUTE)
+ORDER BY exchangeme.accounts.lastlogin DESC;
