6c5d3f7d04ba48a024d6289f8e6548dfbadbb25c
Unknown <supra4keanu@hotmail.com>
2018-07-17 02:35:35 +1000

    Friends improvements

--- ajax/inc/users.inc.profile.php
+++ ajax/inc/users.inc.profile.php
@@ -15,6 +15,8 @@ require_once('../../mysql.php');
 if(
     isset($_POST['User'])
 &&  !empty($_POST['User'])
+&&  isset($_POST['CurrentUser'])
+&&  !empty($_POST['CurrentUser'])
 ) {
 
     // Query
@@ -86,19 +88,67 @@ if(
 
     }
 
+    // Query
+    $QUERY_CHECK_RELATIONSHIP = mysqli_query($conn, "
+    
+    SELECT *
+    FROM exchangeme.friends
+    WHERE exchangeme.friends.userid = (
+        SELECT
+        exchangeme.accounts.id
+        FROM exchangeme.accounts
+        WHERE exchangeme.accounts.username = '" . strip_tags($_POST["CurrentUser"]) . "'
+        AND exchangeme.accounts.session = '" . $_COOKIE['loggedin'] . "'
+    )
+    AND exchangeme.friends.friendid = (
+        SELECT
+        exchangeme.accounts.id
+        FROM exchangeme.accounts
+        WHERE exchangeme.accounts.username = '" . strip_tags($_POST['User']) . "'
+    );
+    
+    ");
+
+    // Fetch Results
+    $RESULT_CHECK_RELATIONSHIP = mysqli_fetch_array($QUERY_CHECK_RELATIONSHIP);
+
     echo "
     
         <div class='table users-table action-container'>
-        
-            <div id='add-user' class='table users-table action-wrapper' data-user='" . $_POST['User'] . "'>
-
-                <object class='table users-table add-user' data='img/materialdesign/ic_add_24px.svg' type='image/svg+xml'></object>
-
-            </div>
-
-            <div id='block-user' class='table users-table action-wrapper' data-user='" . $_POST['User'] . "'>
             
-                <object class='table users-table block-user block-user' data='img/materialdesign/ic_remove_24px.svg' type='image/svg+xml'></object>
+        ";
+
+            if(mysqli_num_rows($QUERY_CHECK_RELATIONSHIP) == 0) {
+
+                echo "
+                
+                    <div id='add-user' class='table users-table action-wrapper' data-user='" . $_POST['User'] . "' title='Add Friend'>
+
+                        <object class='table users-table add-user' data='img/materialdesign/ic_add_24px.svg' type='image/svg+xml'></object>
+
+                    </div>
+
+                ";
+
+            } else {
+
+                echo "
+
+                    <div id='remove-user' class='table users-table action-wrapper' data-user='" . $_POST['User'] . "' title='Remove Friend'>
+
+                        <object class='table users-table remove-user' data='img/materialdesign/ic_remove_24px.svg' type='image/svg+xml'></object>
+
+                    </div>
+
+                ";
+
+            }
+
+        echo "
+
+            <div id='block-user' class='table users-table action-wrapper' data-user='" . $_POST['User'] . "' title='Block User'>
+            
+                <object class='table users-table block-user block-user' data='img/materialdesign/ic_voice_over_off_24px.svg' type='image/svg+xml'></object>
 
             </div>
 
