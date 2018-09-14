18aaa6d031fc4027b64ea5e1b5a07c2665c15109
Unknown <supra4keanu@hotmail.com>
2018-09-14 20:08:21 +1000

    Start of file licensing and comment changes

--- ajax/newsLoad.js
+++ ajax/newsLoad.js
@@ -1,6 +1,6 @@
 // Unapproved use and redistribution of this code and respective product is strictly prohibited.
 // Copyright© 2018 Keanu Ashwell all rights are reserved to the author, creator, registered
-// and licensed owners of this product and it's content
+// authorised and licensed owners of this product and it's content
 
 // jQuery
 
--- mysql.php
+++ mysql.php
@@ -2,7 +2,7 @@
 
 // Unapproved use and redistribution of this code and respective product is strictly prohibited.
 // Copyright© 2018 Keanu Ashwell all rights are reserved to the author, creator, registered 
-// and licensed owners of this product and it's content
+// authorised and licensed owners of this product and it's content
 
 // Declare and define the database variables
 $USERNAME = 'root';
