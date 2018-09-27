c2fd5c5c0cf551c346369e2717fb2f4611407826
Unknown <supra4keanu@hotmail.com>
2018-09-27 14:54:32 +1000

    Removed buy and forum links

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ require_once('mysql.php');
                             <path d='M0 0h24v24H0z' fill='none'></path>
                         </svg>
 
-                            <p class='text center white'><a href='exchange.php'>Buy</a>
+                            <!--<p class='text center white'><a href='exchange.php'>Buy</a>
                             </p>
 
                         <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
@@ -114,7 +114,7 @@ require_once('mysql.php');
                         <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
                             <path d='M0 0h24v24H0z' fill='none'></path>
                             <path d='M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9H9V9h10v2zm-4 4H9v-2h6v2zm4-8H9V5h10v2z' fill='#00FF22'></path>
-                        </svg>
+                        </svg>-->
 
                             <p class='text center white'><a href='about.php'>About</a>
                             </p>
