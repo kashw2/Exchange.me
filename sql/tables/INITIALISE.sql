943a28f3b8c8a74e5bbeb5e0314cec3d63f6dc2e
Unknown <supra4keanu@hotmail.com>
2018-09-28 22:15:40 +1000

    Insert ban reasons, default news and permissions

--- sql/tables/INITIALISE.sql
+++ sql/tables/INITIALISE.sql
@@ -44,7 +44,140 @@ INSERT INTO exchangeme.accounts (
     6,
     "Default Administrator Account",
     "Default Site Administrator"
-)
+);
+
+INSERT INTO exchangeme.banreasons (
+    exchangeme.banreasons.id,
+    exchangeme.banreasons.reason
+) VALUES (
+    DEFAULT,
+    "Custom / Undisclosed"
+);
+
+INSERT INTO exchangeme.banreasons (
+    exchangeme.banreasons.id,
+    exchangeme.banreasons.reason
+) VALUES (
+    DEFAULT,
+    "Hacking"
+);
+
+INSERT INTO exchangeme.banreasons (
+    exchangeme.banreasons.id,
+    exchangeme.banreasons.reason
+) VALUES (
+    DEFAULT,
+    "Spamming"
+);
+
+INSERT INTO exchangeme.banreasons (
+    exchangeme.banreasons.id,
+    exchangeme.banreasons.reason
+) VALUES (
+    DEFAULT,
+    "Malicious Intent"
+);
+
+INSERT INTO exchangeme.banreasons (
+    exchangeme.banreasons.id,
+    exchangeme.banreasons.reason
+) VALUES (
+    DEFAULT,
+    "Harassment"
+);
+
+INSERT INTO exchangeme.banreasons (
+    exchangeme.banreasons.id,
+    exchangeme.banreasons.reason
+) VALUES (
+    DEFAULT,
+    "Racism"
+);
+
+INSERT INTO exchangeme.banreasons (
+    exchangeme.banreasons.id,
+    exchangeme.banreasons.reason
+) VALUES (
+    DEFAULT,
+    "Trolling"
+);
+
+INSERT INTO exchangeme.banreasons (
+    exchangeme.banreasons.id,
+    exchangeme.banreasons.reason
+) VALUES (
+    DEFAULT,
+    "Foul Language"
+);
+
+INSERT INTO exchangeme.news (
+    exchangeme.news.id,
+    exchangeme.news.userid,
+    exchangeme.news.date,
+    exchangeme.news.content
+) VALUES (
+    DEFAULT,
+    0,
+    DEFAULT,
+    "Welcome to Exchange.me!"
+);
+
+INSERT INTO exchangeme.permissions (
+    exchangeme.permissions.id,
+    exchangeme.permissions.name
+) VALUES (
+    DEFAULT,
+    "Banned"
+);
+
+INSERT INTO exchangeme.permissions (
+    exchangeme.permissions.id,
+    exchangeme.permissions.name
+) VALUES (
+    DEFAULT,
+    "Guest"
+);
+
+INSERT INTO exchangeme.permissions (
+    exchangeme.permissions.id,
+    exchangeme.permissions.name
+) VALUES (
+    DEFAULT,
+    "Registered"
+);
+
+INSERT INTO exchangeme.permissions (
+    exchangeme.permissions.id,
+    exchangeme.permissions.name
+) VALUES (
+    DEFAULT,
+    "Member"
+);
+
+INSERT INTO exchangeme.permissions (
+    exchangeme.permissions.id,
+    exchangeme.permissions.name
+) VALUES (
+    DEFAULT,
+    "Moderator"
+);
+
+INSERT INTO exchangeme.permissions (
+    exchangeme.permissions.id,
+    exchangeme.permissions.name
+) VALUES (
+    DEFAULT,
+    "Administrator"
+);
+
+INSERT INTO exchangeme.permissions (
+    exchangeme.permissions.id,
+    exchangeme.permissions.name
+) VALUES (
+    DEFAULT,
+    "Site-Administrator"
+);
+
 
 # Password = password.01
 # Salt = >W'q+DgN.!C3p*]
